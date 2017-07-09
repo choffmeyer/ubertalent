822f56f96ea87b088b6faaf6eab31f5fe4cd4137
Alexey1100 <alexey@MacBook-Air.local>
2017-07-09 23:51:30 +0500

    Job search added, 404 page added

--- bin/ubertalent.rb
+++ bin/ubertalent.rb
@@ -30,6 +30,17 @@ helpers do
       latest_jobs_paged: Job.where(published: 'true').desc('_id').to_a.each_slice(20).to_a
     }
   end
+
+  def jobs_page_data_filtered(title: nil)
+    {
+      latest_jobs_paged: Job.where(published: 'true').where(job_title: /#{title}/i).desc('_id').to_a.each_slice(20).to_a
+    }
+  end
+
+  def add_page_to_fullpath(page_number, fullpath)
+    fullpath = fullpath + '?' unless fullpath.include? '?'
+    fullpath = fullpath.sub(/&page=\d+/, '') + "&page=#{page_number}"
+  end
 end
 
 #### Routes
@@ -40,10 +51,15 @@ get '/' do
 end
 
 get '/jobs' do
-  @page_n = (params[:page] ? params[:page] : 1).to_i
-  @query  = params[:query]
-  @data   = jobs_page_data
-  pass if @page_n < 1
-  pass if @page_n > @data[:latest_jobs_paged].size
+  @query        = params[:query].to_s
+  @data         = (@query.empty? ? jobs_page_data : jobs_page_data_filtered(title: @query))
+  @current_page = (params[:page] ? params[:page] : 1).to_i
+  @total_pages  = @data[:latest_jobs_paged].size
+  pass if @current_page < 1
+  pass if @current_page > @total_pages
   erb :jobs
+end
+
+not_found do
+  erb 404.to_s.to_sym
 end
